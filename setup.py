5c82dc7ba69e480c544938cf3cb0239899d84531
Роман Степанов <r.stepanov@bars-open.ru>
2017-06-16 13:44:50 +0300

    Version 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='hitsl_utils',
-    version='0.2.1',
+    version='0.2.2',
     url='https://stash.bars-open.ru/scm/medvtr/hitsl.utils.git',
     author='hitsl',
     description='Small utilities used in HITSL',
